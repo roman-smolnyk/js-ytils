94eda8515267ab0c36b26dfd66d4de8078408d84
Roman Smolnyk <roman.smolnyk@globallogic.com>
2025-03-30 21:26:35 +0300

    Add Log class

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # js-ytils
+
+```
+https://cdn.jsdelivr.net/gh/roman-smolnyk/js-ytils/ytils.js
+```
+```
+https://cdn.jsdelivr.net/gh/roman-smolnyk/js-ytils/ytils.min.js
+```
---
+++ ytils.js
@@ -0,0 +1,39 @@
+export class Log {
+  static ERROR_LEVEL = 0;
+  static WARN_LEVEL = 1;
+  static INFO_LEVEL = 2;
+  static DEBUG_LEVEL = 3;
+  static TRACE_LEVEL = 4;
+  // _MAP[0] num always converts to string
+  static _MAP = { 0: "error", 1: "warn", 2: "log", 3: "debug", 4: "trace" };
+
+  constructor(level = Log.INFO) {
+    this.CURRENT_LEVEL = level;
+  }
+
+  _stdout = (level, ...messages) => {
+    if (level <= this.CURRENT_LEVEL) {
+      console[Log._MAP[this.CURRENT_LEVEL]](...messages);
+    }
+  };
+
+  error = (...messages) => {
+    this._stdout(Log.ERROR_LEVEL, ...messages);
+  };
+  warn = (...messages) => {
+    this._stdout(Log.WARN_LEVEL, ...messages);
+  };
+  info = (...messages) => {
+    this._stdout(Log.INFO_LEVEL, ...messages);
+  };
+  debug = (...messages) => {
+    this._stdout(Log.DEBUG_LEVEL, ...messages);
+  };
+  trace = (...messages) => {
+    this._stdout(Log.TRACE_LEVEL, ...messages);
+  };
+  // For cases when misswapped .info and .log
+  log = (...messages) => {
+    this._stdout(Log.INFO_LEVEL, ...messages);
+  };
+}
